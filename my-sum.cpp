0305c4fab01f0c889ea6cf2c8d1fb0e359c8cb9c
Ben <73865293+bprhee01@users.noreply.github.com>
2025-03-03 13:44:26 -0600

    initilize segment and arrays

--- my-sum.cpp
+++ my-sum.cpp
@@ -1,30 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*
+Ben Rhee
+OS assignment 1
+Prefix Sum parrallelized through hillis and steele algorithm
+DO "./my-sum numElems numCores input_file output_file"
+*/
+using namespace std;
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <cstring>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
 #include <sys/wait.h>
-#include <math.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
 
 
-//shared data
-typedef struct {
+// Barrier class
+class Barrier{
+private:
+    int current_process_count;
+    int total_process_count;
+public:
+    Barrier(int m){
+        total_process_count = m;
+        current_process_count = 0;
+    }
+    void arrive_and_wait(int pid){
 
-} SharedMemory;
+    }
+};
 
 
-//reusable barrier
 
-void reusableBarrier() {
+// shared data structure
+struct SharedMemory {
+    int n;
+    int m;
+    int *A;
+    int *B;
+    Barrier *barrier;
+};
 
-}
-
-
-//prefix sum 
-void prefixSum() {
-
-}
+//function prototypes
+void process_input_file(const string &input_file, int *A, int n);
 
 
 // main
@@ -45,5 +64,51 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    //  init shared memory
+    size_t size_segment = sizeof(SharedMemory) + (2 * n * sizeof(double)); // shared mem struct + size of the two arrays
+    // create shared memory segment
+    int shmid = shmget(IPC_PRIVATE, size_segment, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR); // private for segments that know ID, size_segment, create segment and give read/write perms
+    
+    if (shmid == -1){
+        fprintf(stderr, "Error: segment creation failed.\n");
+        exit(1);
+    }
+
+    //attach segment
+    SharedMemory *shared_memory = (SharedMemory*)shmat( shmid, nullptr, 0); //attaches schmat segment with id
+
+    if (shared_memory == (void*)-1) {
+        fprintf(stderr, "Error: segment attachment failed.\n");
+        shmctl(shmid, IPC_RMID, nullptr);
+        exit(1);
+    }
+
+    // setup memory address pointers
+    shared_memory->A = (int*)((char*)shared_memory + sizeof(SharedMemory)); //skip of SharedMemory struct 
+    shared_memory->B = shared_memory->A + n;
+
+    //initiliaze shared_memory variables
+    shared_memory->n = n;
+    shared_memory->m = m;
+    shared_memory->barrier = new Barrier(m);
+
+
+    //read input file
+    process_input_file(input_file,shared_memory->A,n);
+
+    //initialize B as A for now
+    for (int i = 0; i < n; i++) {
+        shared_memory->B[i] = shared_memory->A[i];
+    }
+    
+    //as of now the segment looks like [SharedData struct][Array A (n elements)][Array B (n elements)]
     return 0;
+}
+
+void process_input_file(const string &input_file, int *A, int n) {
+    ifstream input_file_stream(input_file);
+    for (int i = 0; i < n; i++){
+        input_file_stream >> A[i];
+    }
+    input_file_stream.close();
 }
